81b9ecb41c133fb79c8cfb73535621ef025d7cef
rabbitracing <100345231+rabbitracing@users.noreply.github.com>
2025-03-06 17:32:46 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Cuma mau bilang", "ACA CANTIK BANGET", "ACA MANIS BANGET", "WOPYOUUUUUU ACAAAA❤💕"],
+        typeSpeed: 70
+    });
+}

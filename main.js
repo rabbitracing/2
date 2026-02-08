8de92ed362c2cb63eba64cedd0c154e44aec2ed6
rabbitracing <100345231+rabbitracing@users.noreply.github.com>
2026-02-08 22:52:11 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ function clicks() {
     $('#div2').addClass("fadeinv2");
         
     var typed = new Typed('.katakata', {
-        strings: ["Just Wanna Say", "Meiii you are so beautiful💖", "You're have the sweetiest smile i ever see💗", "ROORRRRR💖💗💖💗💖💗"],
+        strings: ["Just Wanna Say", "You're so beautiful💖", "You're have the sweetiest smile i ever see💗", "ROORRRRR💖💗💖💗💖💗"],
         typeSpeed: 70
     });
 }
+
 
 
 
